9697e01508415f38a9d521fa99f2777f031e0822
Notionl <145482925+Notionl@users.noreply.github.com>
2023-11-09 19:45:18 -0800

    Update login.php

---
+++ login.php
@@ -0,0 +1,25 @@
+ */
+# Nota: no estamos haciendo validaciones
+$usuario = $_POST["usuario"];
+$palabra_secreta = $_POST["palabra_secreta"];
+
+# Luego de haber obtenido los valores, ya podemos comprobar:
+if ($usuario === $usuario_correcto && $palabra_secreta === $palabra_secreta_correcta) {
+    # Significa que coinciden, así que vamos a guardar algo
+    # en el arreglo superglobal $_SESSION, ya que ese arreglo
+    # "persiste" a través de todas las páginas
+
+    # Iniciar sesión para poder usar el arreglo
+    session_start();
+    # Y guardar un valor que nos pueda decir si el usuario
+    # ya ha iniciado sesión o no. En este caso es el nombre
+    # de usuario
+    $_SESSION["usuario"] = $usuario;
+
+    # Luego redireccionamos a la página "Secreta"
+    header("Location: secreta.php");
+} else {
+    # No coinciden, así que simplemente imprimimos un
+    # mensaje diciendo que es incorrecto
+    echo "El usuario o la contraseña son incorrectos";
+}
